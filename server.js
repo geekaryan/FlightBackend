47ccde6b44870e31b5beb64e9d2c38b2b71f90da
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-12 20:06:26 +0530

    database connected

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const port = 7000;
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
