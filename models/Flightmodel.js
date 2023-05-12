16adf23bad3d88a2d688693f6ae57b2087f50597
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-12 22:29:37 +0530

    model for flight system

---
+++ models/Flightmodel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const flightSchema = new mongoose.model({
+  source: {
+    type: String,
+    require: [true, "source must be present"],
+  },
+  destination: {
+    type: String,
+    require: [true, "destination must be present"],
+  },
+  price: {
+    type: Number,
+    require: [true, "price must be present so that you have to pay"],
+  },
+  flights: {
+    type: String,
+    require: [true, "Flights name must be there."],
+  },
+  Date: {
+    type: Date,
+    required: [true, "Date must be there"],
+  },
+});
+
+const Flight = mongoose.model("Flight", flightSchema);
+
+module.exports = Flight;
