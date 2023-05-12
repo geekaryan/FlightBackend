e07815a18da89f8f7bfdf50c99d101840c55ff87
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-13 04:22:47 +0530

    adding create and findById

--- controllers/Flightcontroller.js
+++ controllers/Flightcontroller.js
@@ -1,7 +1,7 @@
 const { data } = require("@tensorflow/tfjs-node");
 const Flight = require("./../models/Flightmodel");
 
-//getting the flights
+//getting all  the flights
 exports.getFlight = async (req, res) => {
   try {
     const flights = await Flight.find();
@@ -10,6 +10,44 @@ exports.getFlight = async (req, res) => {
       length: flights.length(),
       data: {
         flights,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+//getting one flight
+exports.findByOne = async (req, res) => {
+  try {
+    const flight = await Flight.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      length: flight.length,
+      data: {
+        flight,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+//making the fligts
+
+exports.createFlight = async (req, res) => {
+  try {
+    const flight = await Flight.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        flight,
       },
     });
   } catch (err) {
