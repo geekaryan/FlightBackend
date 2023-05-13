ecb562791253cbf1c043f57523b1a633049de8a6
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-13 19:48:47 +0530

    route /flight

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const FlightRoute = require("./routes/Flightroutes");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api/flight", FlightRoute);
 
 module.exports = app;
